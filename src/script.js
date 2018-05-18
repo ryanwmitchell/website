f3fa19e4ed14d894984c18e0dcea057b51b56c0e
Ben Haynes <ben@rngr.org>
2018-05-18 17:26:21 -0400

    Products page

--- src/script.js
+++ src/script.js
@@ -1,8 +1,19 @@
 $(document).ready(function(){
 	$('.testimonials').slick({
-		adaptiveHeight: true,
+		autoplay: true,
 		autoplaySpeed: 5000,
+		adaptiveHeight: true,
 		dots: true,
+		prevArrow: '<i class="material-icons prev arrow">arrow_back</i>',
+		nextArrow: '<i class="material-icons next arrow">arrow_forward</i>'
+	});
+
+	$('.extensions').slick({
+		autoplay: true,
+		autoplaySpeed: 5000,
+		centerMode: true,
+		centerPadding: '50px',
+		slidesToShow: 3,
 		prevArrow: '<i class="material-icons prev arrow">arrow_back</i>',
 		nextArrow: '<i class="material-icons next arrow">arrow_forward</i>'
 	});
